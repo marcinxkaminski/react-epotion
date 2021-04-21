fc3affc611852922c9f3843df2e5fecc56296b5e
Marcin Kaminski <marcin.kaminski@schibsted.com>
2021-04-21 21:56:18 +0200

    🐛 another bugfix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import Epotion from './components/Epotion';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <Epotion />
-  </React.StrictMode>,
-  document.querySelector('#root'),
-);
+export { default } from './Epotion';
